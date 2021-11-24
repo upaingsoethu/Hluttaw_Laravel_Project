a08448ff5ec60b6ed83804f8863ceb790e125d18
paingsoethu <kopaingsoethu2015@gmail.com>
2021-11-24 15:47:25 +0630

    view lesson commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+</head>
+<body>
+<h1>This Contact Page</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('about', function () {
+    return view('about');
+});
+Route::get('contact', function () {
+    return view('contact');
+});
+
+Route::get('ourproducts', function () {
+    return view('productlist.productlist');
+});
